52bf23c6bd8e22c1f37184d2889a4ea2d8c5bdbf
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-02-13 14:15:17 +0000

    commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+    skip_before_action :login_required, only: [:new, :create]
+    before_action :correct_user, only: [:show, :edit, :destroy]
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          log_in(@user)
+          redirect_to tasks_path
+        else
+          render :new
+        end
+      end
+
+      def show
+        @user = User.find(params[:id])
+      end
+
+      def edit
+        @user = User.find(params[:id])
+      end
+
+      def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to new_session_path
+      end
+    
+      private
+    
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+
+      def correct_user
+        @user = User.find(params[:id])
+        redirect_to current_user unless current_user?(@user)
+      end
+end
