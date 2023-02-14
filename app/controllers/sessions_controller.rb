f425d91801f29ffeb5ad22e7828c1e533605454e
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-02-14 07:41:49 +0000

    commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             log_in(user)
-            redirect_to user_path(user.id)
+            redirect_to tasks_path
           else
             flash.now[:danger] = 'Je n\'ai pas réussi à me connecter'
            render :new
