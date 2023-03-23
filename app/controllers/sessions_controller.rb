fff5683d844606fd82a0d9bd9099bdf40e5f8e97
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-03-23 10:13:04 +0000

    commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,19 +6,18 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params[:session][:email].downcase)
-        if user && user.authenticate(params[:session][:password])
+        if user&.authenticate(params[:session][:password])
             log_in(user)
-            flash[:notice] = 'Vous êtes maintenant connecté'
-            redirect_to tasks_path
-          else
-            flash.now[:danger] = 'Adresse email ou mot de passe incorrect'
-           render :new
-          end
+            redirect_to tasks_path, notice: 'ログインしました'
+        else
+            flash.now[:alert] = 'メールアドレスまたはパスワードに誤りがあります'
+            render :new
+        end
     end
 
     def destroy
         session.delete(:user_id)
-        flash[:notice] = 'Vous avez été déconnecté.'
+        flash[:notice] = 'ログアウトしました'
         redirect_to new_session_path
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,52 @@
 class UsersController < ApplicationController
-    skip_before_action :login_required, only: [:new, :create]
-    before_action :correct_user, only: [:show, :edit, :destroy]
+  before_action :set_user, only: %i(show edit update destroy correct_user)
+  skip_before_action :login_required, only: %i(new create)
+  before_action :correct_user, only: %i(show edit update destroy)
 
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      redirect_to tasks_path, notice: 'アカウントを登録しました'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'アカウントを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to new_session_path
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
     end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-          log_in(@user)
-          flash[:notice] = 'Compte Enregistré'
-          redirect_to tasks_path
-        else
-          render :new
-        end
-      end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
-      def show
-        @user = User.find(params[:id])
-      end
-
-      def edit
-        @user = User.find(params[:id])
-      end
-
-      def update
-        @user = User.find(params[:id])
-        if @user.update(user_params)
-          flash[:notice] = 'Compte mis à jour'
-          redirect_to user_path(@user.id)
-        else
-          render :edit
-        end
-      end
-
-      def destroy
-        @user = User.find(params[:id])
-        @user.destroy
-        flash[:notice] = 'Compte supprimé'
-        redirect_to new_session_path
-      end
-    
-      private
-    
-      def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-      end
-
-      def correct_user
-        @user = User.find(params[:id])
-        redirect_to current_user unless current_user?(@user)
-      end
+    def correct_user
+      redirect_to current_user unless current_user?(@user)
+    end
 end
